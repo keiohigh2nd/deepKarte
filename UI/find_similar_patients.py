c9889a0a5f42a246bc2277849af39c28c443a3fd
keiohigh2nd@gmail.com <keiohigh2nd@gmail.com>
2016-07-07 18:17:18 +0900

    view

--- UI/find_similar_patients.py
+++ UI/find_similar_patients.py
@@ -15,17 +15,17 @@ class Example(QtGui.QMainWindow):
         
     def initUI(self):      
 
-	btn = QtGui.QPushButton("Find", self)
-        btn.move(5, 50)
-        btn.clicked.connect(self.buttonClicked)
+	btn1 = QtGui.QPushButton("Find", self)
+        btn1.move(5, 50)
+        btn1.clicked.connect(self.buttonClicked1)
 
-	btn1 = QtGui.QPushButton("Stat", self)
-        btn1.move(100, 50)
-        btn1.clicked.connect(self.buttonClicked)
+	btn2 = QtGui.QPushButton("Stat", self)
+        btn2.move(100, 50)
+        btn2.clicked.connect(self.buttonClicked2)
 
-	btn2 = QtGui.QPushButton("Treatment", self)
-        btn2.move(195, 50)
-        btn2.clicked.connect(self.buttonClicked)
+	btn3 = QtGui.QPushButton("Treatment", self)
+        btn3.move(195, 50)
+        btn3.clicked.connect(self.buttonClicked3)
       
 	self.lbl = QtGui.QLabel(self)
         self.statusBar()
@@ -34,7 +34,7 @@ class Example(QtGui.QMainWindow):
         self.setWindowTitle('Alpha Karte')
         self.show()
         
-    def buttonClicked(self, text):
+    def buttonClicked1(self, text):
 	text, ok = QtGui.QInputDialog.getText(self, 'Input Dialog', 'Enter Patient Id')
 	#In this function, you can pass text from str(text)
 	#類似患者検索アルゴリズムの2つ
@@ -48,8 +48,46 @@ class Example(QtGui.QMainWindow):
 
 	self.lbl.move(10, 10)
         #self.lbl.setText('<a href="http://0.0.0.0:5601/app/kibana#/doc/test/test/karte?id=%s">Stackoverflow/</a>'%pid)
-        self.lbl.setText('<a href="file:///Users/keiohigh2nd/deepKarte/view/donut.html">Stackoverflow/</a>')
+        self.lbl.setText('<a href="file:///Users/keiohigh2nd/deepKarte/view/cumulativeLineChart.html">Patient Course/</a>')
         self.lbl.setOpenExternalLinks(True)
+
+
+    def buttonClicked2(self, text):
+        text, ok = QtGui.QInputDialog.getText(self, 'Input Dialog', 'Enter Patient Id')
+        #In this function, you can pass text from str(text)
+        #類似患者検索アルゴリズムの2つ
+        #近傍行列に基づくもの
+        #similar_pid = select_similar.find_similar_patient(text)
+
+        #病名とBoGを利用した共起表現の所
+        p_index = select_similar.return_patient_index(text)
+        extract_P.show_noun(p_index)
+        #self.statusBar().showMessage(similar_pid + ' is Similar')
+
+        self.lbl.move(10, 10)
+        #self.lbl.setText('<a href="http://0.0.0.0:5601/app/kibana#/doc/test/test/karte?id=%s">Stackoverflow/</a>'%pid)
+        self.lbl.setText('<a href="file:///Users/keiohigh2nd/deepKarte/view/sunburst.html">Donuts</a>')
+        self.lbl.setOpenExternalLinks(True)
+
+
+    def buttonClicked3(self, text):
+        text, ok = QtGui.QInputDialog.getText(self, 'Input Dialog', 'Enter Patient Id')
+        #In this function, you can pass text from str(text)
+        #類似患者検索アルゴリズムの2つ
+        #近傍行列に基づくもの
+        #similar_pid = select_similar.find_similar_patient(text)
+
+        #病名とBoGを利用した共起表現の所
+        p_index = select_similar.return_patient_index(text)
+        extract_P.show_noun(p_index)
+        #self.statusBar().showMessage(similar_pid + ' is Similar')
+
+        self.lbl.move(10, 10)
+        #self.lbl.setText('<a href="http://0.0.0.0:5601/app/kibana#/doc/test/test/karte?id=%s">Stackoverflow/</a>'%pid)
+        self.lbl.setText('<a href="file:///Users/keiohigh2nd/deepKarte/view/parallelCoordinatesChart.html">Similar Patients</a>')
+        self.lbl.setOpenExternalLinks(True)
+
+
 
 
 def main():

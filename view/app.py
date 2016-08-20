7ddac6e36c3a9cee587f3db525167f6cf1ab1e2e
keiohigh2nd@gmail.com <keiohigh2nd@gmail.com>
2016-08-21 00:45:08 +0900

    modify template files

--- view/app.py
+++ view/app.py
@@ -25,33 +25,44 @@ f.write(htmlSIM)
 f.close()
 
 #cumulative chart部分
+f = open("view/json/cumu.json", 'r')
+texts = f.read()
+f.close()
 tmpCUM1 = env.get_template('view/template/temp_cumulativeLineChart.html')
-htmlCUM1 = tmpCUM1.render(label="CRP")
+htmlCUM1 = tmpCUM1.render(label="CRP", json=texts)
 
 f = codecs.open('view/o_CUM1.html', 'w', 'utf-8')
 f.write(htmlCUM1)
 f.close()
 
 #cumulative chart部分
+f = open("view/json/cumu.json", 'r')
+texts = f.read()
+f.close()
 tmpCUM2 = env.get_template('view/template/temp_cumulativeLineChart.html')
-htmlCUM2 = tmpCUM2.render(label="CRP")
+htmlCUM2 = tmpCUM2.render(label="CRP", json=texts)
 
 f = codecs.open('view/o_CUM2.html', 'w', 'utf-8')
 f.write(htmlCUM2)
 f.close()
 
 #sunburst部分
+f = open("view/json/d_sunburst.json", 'r')
+texts = f.read()
+f.close()
 tmpSUN = env.get_template('view/template/temp_sunburst.html')
-htmlSUN = tmpSUN.render(label="CRP")
+htmlSUN = tmpSUN.render(label="CRP", json=texts.decode('utf-8'))
 
 f = codecs.open('view/o_SUN.html', 'w', 'utf-8')
 f.write(htmlSUN)
 f.close()
 
 #parallelCordinates
-#multi_labels = ["Stage", "BP", "CRP", "HR", "Tempeture", "Weight", "Age"]
+f = open("view/json/d_parallelCordinates.json", 'r')
+texts = f.read()
+f.close() 
 tmpPAR = env.get_template('view/template/temp_parallelCoordinates.html')
-htmlPAR = tmpPAR.render(label= ["Stage", "BP", "CRP", "HR", "Tempeture", "Weight", "Age"])
+htmlPAR = tmpPAR.render(label= ["Stage", "BP", "CRP", "HR", "Tempeture", "Weight", "Age"], json=texts)
 
 f = codecs.open('view/o_PAR.html', 'w', 'utf-8')
 f.write(htmlPAR)
